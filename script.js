7bdbcf455d2b4da107c93c2a5c711f0795a676ea
tinuh <tinuvanapamula@gmail.com>
2020-04-15 22:31:30 -0400

    Changes

---
+++ script.js
@@ -0,0 +1,12 @@
+function embed(){
+    var link = String(window.document.forms[0].link.value);
+
+    var video_id = link.split('v=')[1];
+    var ampersandPosition = video_id.indexOf('&');
+    if(ampersandPosition != -1) {
+        video_id = video_id.substring(0, ampersandPosition);
+    }
+
+    var redirect = "https://youtube.com/embed/" + video_id;
+    window.location.href = redirect;
+}
