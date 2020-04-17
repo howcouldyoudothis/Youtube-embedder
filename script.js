48d647d4d5b57b552e604c802ae731c4c3b3f5aa
tinuh <tinuvanapamula@gmail.com>
2020-04-17 17:02:50 -0400

    Added corona, and mobile corona functionality.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function embed(){
     var link = String(window.document.forms[0].link.value);
-
+    if (link == "corona"){
+        window.open("http://redirect.tinu.tech/corona");
+    }
+    else if(link == "mobilecorona"){
+        window.open("http://redirect.tinu.tech/mobilecorona");
+    }
+    else{
     var video_id = link.split('v=')[1];
     var ampersandPosition = video_id.indexOf('&');
     if(ampersandPosition != -1) {
@@ -10,5 +16,5 @@ function embed(){
     var redirect = "http://youtube.com/embed/" + video_id;
     window.open(redirect);
     window.close();
-
+    }
 }
