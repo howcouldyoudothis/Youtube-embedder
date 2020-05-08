68100e9f4cdd72e2ccbb3fadb11c95fb5655fe5c
tinuh <tinuvanapamula@gmail.com>
2020-05-07 22:00:53 -0400

    Significant Changes to the Youtube Embeder. Added youtu.be functionality.

--- script.js
+++ script.js
@@ -6,18 +6,43 @@ function embed(){
     else if(link == "mobilecorona"){
         window.open("http://redirect.tinu.tech/mobilecorona");
     }
-    else{
-    var video_id = link.split('v=')[1];
-    try {
-        var ampersandPosition = video_id.indexOf('&');
-        if (ampersandPosition != -1) {
-            video_id = video_id.substring(0, ampersandPosition);
-        }
-    }
-    catch{
-    }
+    else {
+        var valid = true;
+        var video_id = link.split('v=');
 
-    var redirect = "http://youtube.com/embed/" + video_id;
-    location.href = redirect;
+        if (String(link) == String(video_id[0])) {
+            if (link.includes('youtu.be')){
+                var video_id = link.split('youtu.be/');
+            }
+            else if (link.includes('embed')){
+                var video_id = link.split('embed/');
+            }
+            else{
+                window.alert("Please Enter a Valid Youtube URL!");
+                window.document.forms[0].link.value = "";
+                valid = false;
+            }
+        }
+
+        video_id = video_id[1];
+        if (valid) {
+            try {
+                var ampersandPosition = video_id.indexOf('&');
+                if (ampersandPosition != -1) {
+                    video_id = video_id.substring(0, ampersandPosition);
+                }
+            }
+            catch {
+            }
+
+            var redirect = "http://youtube.com/embed/" + video_id;
+            if (window.document.forms[0].closeTab.checked) {
+                window.open(redirect);
+                window.document.forms[0].link.value = "";
+            }
+            else{
+                window.location.href = redirect;
+            }
+        }
     }
 }
